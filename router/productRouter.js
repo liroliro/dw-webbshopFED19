abf261aba69e37fef44a6bbbf79d8f3b56d731bb
Svanemar <58325572+Svanemar@users.noreply.github.com>
2020-02-28 14:23:40 +0100

    Added error

---
+++ router/productRouter.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const ProductModel = require('../model/product')
+
+
+const router = express.Router();
+
+router.get('/product', (req, res) => {
+    apartment = ProductModel
+    res.render('product', {
+        apartment
+    })
+})
+
+module.exports = { router }
