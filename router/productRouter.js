0b5cd0ffaa9449a9a033a482c55bd707213f3812
Chabbe <henriksson.charlie@gmail.com>
2020-03-03 15:05:59 +0100

    Cant get router.post(/createproduct) to work.

--- router/productRouter.js
+++ router/productRouter.js
@@ -2,44 +2,62 @@ const express = require('express')
 const ProductModel = require('../model/product')
 const router = express.Router();
 
-router.get('/', (req,res) =>{
+router.get('/', (req, res) => {
     let pagination = req.query.page;
     res.render('index');
 })
 
-router.get('/admin', (req,res) =>{
+router.get('/admin', (req, res) => {
     res.render('admin')
 })
 
-router.get('/cart', (req,res) =>{
+router.get('/cart', (req, res) => {
     res.render('cart')
 })
 
-router.get('/checkout', (req,res) =>{
+router.get('/checkout', (req, res) => {
     res.render('checkout')
 })
 
 router.get('/createproduct', (req, res) => {
-    apartment = ProductModel
+    const apartment = ProductModel
     res.render('createproduct', {
         apartment
     })
 })
 
 
-router.get('/contact', (req,res)=>{
+//FUNKAR INTE VVVV
+router.post('/createproduct', async (req, res) => {
+    const apartment = new ProductModel({
+        name: req.body.name,
+        room: req.body.room,
+        price: req.body.price,
+        url: req.body.url
+    })
+
+    await apartment.save();
+    res.redirect('/createproduct')
+
+})
+//FUNKAR INTE ^^^^
+
+
+router.get('/contact', (req, res) => {
     res.render('contact')
 })
 
-router.get('/my-pages', (req,res) =>{
+router.get('/my-pages', (req, res) => {
     res.render('my-pages')
 })
 
 
-router.get('/product', (req,res) =>{
+router.get('/product', async (req, res) => {
     const Items = await ProductModel.find();
-      
-    res.render('product', { Items })
+
+    res.render('product', {
+        Items
+    })
 })
 
 router.post("/product", async (req, res) => {
