550f2b86cd90f06a82a3043025ebdbc4dc5d1af7
Oscar Mattsson <oscarmatttsson@gmail.com>
2020-03-03 12:36:38 +0100

    Added ground structure for Router-file

--- router/productRouter.js
+++ router/productRouter.js
@@ -2,6 +2,23 @@ const express = require('express')
 const ProductModel = require('../model/product')
 const router = express.Router();
 
+router.get('/', (req,res) =>{
+    let pagination = req.query.page;
+    res.render('index');
+})
+
+router.get('/admin', (req,res) =>{
+    res.render('admin')
+})
+
+router.get('/cart', (req,res) =>{
+    res.render('cart')
+})
+
+router.get('/checkout', (req,res) =>{
+    res.render('checkout')
+})
+
 router.get('/createproduct', (req, res) => {
     apartment = ProductModel
     res.render('createproduct', {
@@ -9,17 +26,35 @@ router.get('/createproduct', (req, res) => {
     })
 })
 
+
+router.get('/contact', (req,res)=>{
+    res.render('contact')
+})
+
+router.get('/my-pages', (req,res) =>{
+    res.render('my-pages')
+})
+
+
+router.get('/product', (req,res) =>{
+    const Items = await ProductModel.find();
+      
+    res.render('product', { Items })
+})
+
 router.post("/product", async (req, res) => {
 
-    const newapartment = new ProductModel({
+    const newApartment = new ProductModel({
         name: req.body.name,
         room: req.body.room,
         price: req.body.price,
         url: req.body.url
     })
 
-    const response = await newapartment.save();
-    res.redirect("/product")
+    await newApartment.save((err, suc) => {
+        err ? res.send(err.message) : res.redirect('/product')
+    })
+
 })
 
 module.exports = router;
